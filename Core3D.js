11d7ecd397b3621f29edeb65c851fab366af9c7b
Brian Beyer <bb@ssi-air.(none)>
2011-11-23 09:51:53 -0700

    Fixed resize of globe

--- Core3D.js
+++ Core3D.js
@@ -1,267 +1,280 @@
 var SSI = SSI || {};
 
 SSI.Core3D = function(container) {
-	// Variables used to draw the 3D elements
-	var camera, scene, projector, renderer, w, h;
-	var vector, animate;
+    // Variables used to draw the 3D elements
+    var camera, scene, projector, renderer, w, h;
+    var vector, animate;
 
-	var overRenderer;
+    var overRenderer;
 
-	// Constants for zooming, rotation, etc.
-	var curZoomSpeed = 0;
+    // Constants for zooming, rotation, etc.
+    var curZoomSpeed = 0;
 
-	var mouse = {
-		x : 0,
-		y : 0
-	}, mouseOnDown = {
-		x : 0,
-		y : 0
-	};
-	var rotation = {
-		x : 0,
-		y : 0
-	}, target = {
-		x : Math.PI * 3 / 2,
-		y : Math.PI / 6.0
-	}, targetOnDown = {
-		x : 0,
-		y : 0
-	};
+    var mouse = {
+        x : 0,
+        y : 0
+    }, mouseOnDown = {
+        x : 0,
+        y : 0
+    };
+    var rotation = {
+        x : 0,
+        y : 0
+    }, target = {
+        x : Math.PI * 3 / 2,
+        y : Math.PI / 6.0
+    }, targetOnDown = {
+        x : 0,
+        y : 0
+    };
 
-	// set initial distance
-	var distance = 50000, distanceTarget = 50000;
-	var PI_HALF = Math.PI / 2;
+    // set initial distance
+    var distance = 50000, distanceTarget = 50000;
+    var PI_HALF = Math.PI / 2;
 
-	var maxZoom = 150000;
-	var minZoom = 10000;
+    var maxZoom = 150000;
+    var minZoom = 10000;
 
-	var drawnObjects = new Array();
+    var drawnObjects = new Array();
 
-	function init() {
-		w = container.offsetWidth || window.innerWidth;
-		h = container.offsetHeight || window.innerHeight;
+    var resizeTimeout = null;
 
-		setupRenderer();
+    function init() {
+        w = container.offsetWidth || window.innerWidth;
+        h = container.offsetHeight || window.innerHeight;
 
-		// Field of View (View Angle)
-		// Ratio between width and height, has to match aspect of CanvasRenderer
-		// Near, Far
-		camera = new THREE.PerspectiveCamera(30, w / h, 1, 315000);
+        setupRenderer();
 
-		camera.position.z = distance;
-		vector = new THREE.Vector3();
+        // Field of View (View Angle)
+        // Ratio between width and height, has to match aspect of CanvasRenderer
+        // Near, Far
+        camera = new THREE.PerspectiveCamera(30, w / h, 1, 315000);
 
-		// Scene into which the earth and other objects are displayed
-		scene = new THREE.Scene();
+        camera.position.z = distance;
+        vector = new THREE.Vector3();
 
-		addEventListeners();
+        // Scene into which the earth and other objects are displayed
+        scene = new THREE.Scene();
 
-		animate();
-	}
+        addEventListeners();
 
-	function setupRenderer() {
-		projector = new THREE.Projector();
-		renderer = new THREE.WebGLRenderer({
-			antialias : true
-		});
-		renderer.autoClear = false;
-		renderer.setClearColorHex(0x000000, 0.0);
-		renderer.setSize(w, h);
+        animate();
+    }
 
-		renderer.domElement.style.position = 'absolute';
+    function setupRenderer() {
+        projector = new THREE.Projector();
+        renderer = new THREE.WebGLRenderer({
+            antialias : true
+        });
+        renderer.autoClear = false;
+        renderer.setClearColorHex(0x000000, 0.0);
+        renderer.setSize(w, h);
 
-		container.appendChild(renderer.domElement);
-	}
+        renderer.domElement.style.position = 'absolute';
 
-	function addEventListeners() {
-		// Add event listeners for rotating, zooming, etc.
+        container.appendChild(renderer.domElement);
+    }
 
-		container.addEventListener('mousedown', onMouseDown, false);
+    function addEventListeners() {
+        // Add event listeners for rotating, zooming, etc.
 
-		container.addEventListener('mousewheel', onMouseWheel, false);
+        container.addEventListener('mousedown', onMouseDown, false);
 
-		document.addEventListener('keydown', onDocumentKeyDown, false);
+        container.addEventListener('mousewheel', onMouseWheel, false);
 
-		window.addEventListener('resize', onWindowResize, false);
+        document.addEventListener('keydown', onDocumentKeyDown, false);
 
-		container.addEventListener('mouseover', function() {
-			overRenderer = true;
-		}, false);
+        window.addEventListener('resize', onWindowResize, false);
 
-		container.addEventListener('mouseout', function() {
-			overRenderer = false;
-		}, false);
-	}
+        container.addEventListener('mouseover', function() {
+            overRenderer = true;
+        }, false);
 
-	function animate() {
-		requestAnimationFrame(animate);
-		render();
-	}
+        container.addEventListener('mouseout', function() {
+            overRenderer = false;
+        }, false);
+    }
 
-	function render() {
-		zoom(curZoomSpeed);
+    function animate() {
+        requestAnimationFrame(animate);
+        render();
+    }
+
+    function render() {
+        zoom(curZoomSpeed);
 
         //console.log("target: " + JSON.stringify(target));
-		rotation.x += (target.x - rotation.x) * 0.1;
-		rotation.y += (target.y - rotation.y) * 0.1;
-		distance += (distanceTarget - distance) * 0.3;
+        rotation.x += (target.x - rotation.x) * 0.1;
+        rotation.y += (target.y - rotation.y) * 0.1;
+        distance += (distanceTarget - distance) * 0.3;
 
-		camera.position.x = distance * Math.sin(rotation.x) * Math.cos(rotation.y);
-		camera.position.y = distance * Math.sin(rotation.y);
-		camera.position.z = distance * Math.cos(rotation.x) * Math.cos(rotation.y);
-		camera.lookAt(scene.position);
+        camera.position.x = distance * Math.sin(rotation.x) * Math.cos(rotation.y);
+        camera.position.y = distance * Math.sin(rotation.y);
+        camera.position.z = distance * Math.cos(rotation.x) * Math.cos(rotation.y);
+        camera.lookAt(scene.position);
 
-		vector.copy(camera.position);
+        vector.copy(camera.position);
 
-		scaleDrawnObjects();
+        scaleDrawnObjects();
 
-		renderer.clear();
-		renderer.render(scene, camera);
-	}
+        renderer.clear();
+        renderer.render(scene, camera);
+    }
 
-	function scaleDrawnObjects() {
-		for(var i in drawnObjects) {
-			if(drawnObjects[i].scale == true) {
-				var objectPosition = drawnObjects[i].shape.position;
-				var xDiff = objectPosition.x - camera.position.x;
-				var yDiff = objectPosition.y - camera.position.y;
-				var zDiff = objectPosition.z - camera.position.z;
-				var distanceFromCamera = Math.sqrt(xDiff * xDiff + yDiff * yDiff + zDiff * zDiff);
-				var scaleFactor = distanceFromCamera / (6371 * 7);
-				drawnObjects[i].shape.scale.x = drawnObjects[i].shape.scale.y = drawnObjects[i].shape.scale.z = scaleFactor;
-			}
-		}
-	}
+    function scaleDrawnObjects() {
+        for(var i in drawnObjects) {
+            if(drawnObjects[i].scale == true) {
+                var objectPosition = drawnObjects[i].shape.position;
+                var xDiff = objectPosition.x - camera.position.x;
+                var yDiff = objectPosition.y - camera.position.y;
+                var zDiff = objectPosition.z - camera.position.z;
+                var distanceFromCamera = Math.sqrt(xDiff * xDiff + yDiff * yDiff + zDiff * zDiff);
+                var scaleFactor = distanceFromCamera / (6371 * 7);
+                drawnObjects[i].shape.scale.x = drawnObjects[i].shape.scale.y = drawnObjects[i].shape.scale.z = scaleFactor;
+            }
+        }
+    }
 
-	// Stock Behaviors like rotating and zooming
-	function onMouseDown(event) {
-		event.preventDefault();
+    // Stock Behaviors like rotating and zooming
+    function onMouseDown(event) {
+        event.preventDefault();
 
-		container.addEventListener('mousemove', onMouseMove, false);
-		container.addEventListener('mouseup', onMouseUp, false);
-		container.addEventListener('mouseout', onMouseOut, false);
+        container.addEventListener('mousemove', onMouseMove, false);
+        container.addEventListener('mouseup', onMouseUp, false);
+        container.addEventListener('mouseout', onMouseOut, false);
 
-		mouseOnDown.x = -event.clientX;
-		mouseOnDown.y = event.clientY;
+        mouseOnDown.x = -event.clientX;
+        mouseOnDown.y = event.clientY;
 
-		targetOnDown.x = target.x;
-		targetOnDown.y = target.y;
+        targetOnDown.x = target.x;
+        targetOnDown.y = target.y;
 
-		container.style.cursor = 'move';
-	}
+        container.style.cursor = 'move';
+    }
 
-	function onMouseMove(event) {
-		mouse.x = -event.clientX;
-		mouse.y = event.clientY;
+    function onMouseMove(event) {
+        mouse.x = -event.clientX;
+        mouse.y = event.clientY;
 
-		var zoomDamp = distance / (35000);
+        var zoomDamp = distance / (35000);
 
-		target.x = targetOnDown.x + (mouse.x - mouseOnDown.x) * 0.005 * zoomDamp;
-		target.y = targetOnDown.y + (mouse.y - mouseOnDown.y) * 0.005 * zoomDamp;
+        target.x = targetOnDown.x + (mouse.x - mouseOnDown.x) * 0.005 * zoomDamp;
+        target.y = targetOnDown.y + (mouse.y - mouseOnDown.y) * 0.005 * zoomDamp;
 
-		target.y = target.y > PI_HALF ? PI_HALF : target.y;
-		target.y = target.y < -PI_HALF ? -PI_HALF : target.y;
-	}
+        target.y = target.y > PI_HALF ? PI_HALF : target.y;
+        target.y = target.y < -PI_HALF ? -PI_HALF : target.y;
+    }
 
-	function onMouseUp(event) {
-		event.preventDefault();
+    function onMouseUp(event) {
+        event.preventDefault();
 
-		container.removeEventListener('mousemove', onMouseMove, false);
-		container.removeEventListener('mouseup', onMouseUp, false);
-		container.removeEventListener('mouseout', onMouseOut, false);
-		container.style.cursor = 'auto';
-	}
+        container.removeEventListener('mousemove', onMouseMove, false);
+        container.removeEventListener('mouseup', onMouseUp, false);
+        container.removeEventListener('mouseout', onMouseOut, false);
+        container.style.cursor = 'auto';
+    }
 
-	function onMouseOut(event) {
-		container.removeEventListener('mousemove', onMouseMove, false);
-		container.removeEventListener('mouseup', onMouseUp, false);
-		container.removeEventListener('mouseout', onMouseOut, false);
-	}
+    function onMouseOut(event) {
+        container.removeEventListener('mousemove', onMouseMove, false);
+        container.removeEventListener('mouseup', onMouseUp, false);
+        container.removeEventListener('mouseout', onMouseOut, false);
+    }
 
-	function onMouseWheel(event) {
-		event.preventDefault();
-		if(overRenderer) {
-			zoom(event.wheelDeltaY * (10));
-		}
-		return false;
-	}
+    function onMouseWheel(event) {
+        event.preventDefault();
+        if(overRenderer) {
+            zoom(event.wheelDeltaY * (10));
+        }
+        return false;
+    }
 
-	function onDocumentKeyDown(event) {
-		switch (event.keyCode) {
-			case 38:
-				zoom(3200);
-				event.preventDefault();
-				break;
-			case 40:
-				zoom(-3200);
-				event.preventDefault();
-				break;
-		}
-	}
+    function onDocumentKeyDown(event) {
+        switch (event.keyCode) {
+            case 38:
+                zoom(3200);
+                event.preventDefault();
+                break;
+            case 40:
+                zoom(-3200);
+                event.preventDefault();
+                break;
+        }
+    }
 
-	function onWindowResize(event) {
-		logger.debug('resize');
-		w = container.offsetWidth || window.innerWidth;
+    function onWindowResize(event) {
+        // so right now this event was fired when the entire window was resized,
+        // but the individual dom elements haven't been resized yet. We will wait
+        // a bit then execute the actual resize code so we can use the updated
+        // element sizes.
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(function() {
+            resize();
+        }, 250);
+    }
+
+    function resize() {
+        logger.debug('resize');
+        w = container.offsetWidth || window.innerWidth;
         h = container.offsetHeight || window.innerHeight;
-		camera.aspect = w / h;
-		camera.updateProjectionMatrix();
-		renderer.setSize(w, h);
-	}
+        camera.aspect = w / h;
+        camera.updateProjectionMatrix();
+        renderer.setSize(w, h);
+    }
 
-	function zoom(delta) {
-		distanceTarget -= delta;
-		distanceTarget = distanceTarget > maxZoom ? maxZoom : distanceTarget;
-		distanceTarget = distanceTarget < minZoom ? minZoom : distanceTarget;
-	}
+    function zoom(delta) {
+        distanceTarget -= delta;
+        distanceTarget = distanceTarget > maxZoom ? maxZoom : distanceTarget;
+        distanceTarget = distanceTarget < minZoom ? minZoom : distanceTarget;
+    }
 
-	// Priviledged Methods
-	this.draw = function(id, shape, scale) {
-		if(drawnObjects[id] == undefined) {
-			logger.debug(" adding and drawing: " + id);
-			scene.add(shape);
-			drawnObjects[id] = {
-				shape : shape,
-				scale : scale
-			};
-		}
-	}
+    // Priviledged Methods
+    this.draw = function(id, shape, scale) {
+        if(drawnObjects[id] == undefined) {
+            logger.debug(" adding and drawing: " + id);
+            scene.add(shape);
+            drawnObjects[id] = {
+                shape : shape,
+                scale : scale
+            };
+        }
+    }
 	
-	this.showObject = function(id) {
-		// if object exists in drawnObjects then add back to scene
-		if (drawnObjects[id] != undefined) {
-	        logger.debug("adding shape back to scene for id " + id);
-		    scene.add(drawnObjects[id].shape);	
-		}
-	}
+    this.showObject = function(id) {
+        // if object exists in drawnObjects then add back to scene
+        if (drawnObjects[id] != undefined) {
+            logger.debug("adding shape back to scene for id " + id);
+            scene.add(drawnObjects[id].shape);
+        }
+    }
 	
-	this.hideObject = function(id) {
-		if (drawnObjects[id] != undefined) {
-			logger.debug("removing object from scene with id: " + id);
-			scene.remove(drawnObjects[id].shape);
-		}
-	}
+    this.hideObject = function(id) {
+        if (drawnObjects[id] != undefined) {
+            logger.debug("removing object from scene with id: " + id);
+            scene.remove(drawnObjects[id].shape);
+        }
+    }
 	
-	this.getObjectPosition = function(id) {
-	    return drawnObjects[id].shape.position;
-	}
+    this.getObjectPosition = function(id) {
+        return drawnObjects[id].shape.position;
+    }
 	
-	this.moveCameraTo = function(position_vector) {
-	    // This method converts a position into the rotation coordinate system used to move the camera
-	    // The target.x parameter is the rotation angle from the positive Z axis
-	    // target.y is the rotation angle away from the z-x plane
-	    logger.debug("Moving camera to: " + JSON.stringify(position_vector));
+    this.moveCameraTo = function(position_vector) {
+        // This method converts a position into the rotation coordinate system used to move the camera
+        // The target.x parameter is the rotation angle from the positive Z axis
+        // target.y is the rotation angle away from the z-x plane
+        logger.debug("Moving camera to: " + JSON.stringify(position_vector));
 	    
-	    // sets the distance from the center of the scene the camera will end up
-	    distanceTarget = position_vector.length();
+        // sets the distance from the center of the scene the camera will end up
+        distanceTarget = position_vector.length();
 	    
-	    // unit vectors along the z and y axis 
-	    var zAxisVector = new THREE.Vector3(0,0,1);
-	    var yAxisVector = new THREE.Vector3(0,1,0);
+        // unit vectors along the z and y axis
+        var zAxisVector = new THREE.Vector3(0,0,1);
+        var yAxisVector = new THREE.Vector3(0,1,0);
 	    
-	    // vector that removes the y component of the target vector for purpose of calculating the angle
-	    // between it the input position_vector and the y-z plane
-	    var positionY0Vector = new THREE.Vector3();
-	    positionY0Vector.copy(position_vector);
+        // vector that removes the y component of the target vector for purpose of calculating the angle
+        // between it the input position_vector and the y-z plane
+        var positionY0Vector = new THREE.Vector3();
+        positionY0Vector.copy(position_vector);
         
         // set the y to zero and normalize to unit length
         positionY0Vector.y = 0;
@@ -270,28 +283,28 @@ SSI.Core3D = function(container) {
         //normalize the position_vector to unit length
         position_vector.normalize();
 	    
-	    // calculates the angle between the positive y axis and the input position vector
-	    // then subtract this from 90 degrees to shift it to be from the z-x plane
-	    var y = (Math.PI/2) - Math.acos(yAxisVector.dot(position_vector));
+        // calculates the angle between the positive y axis and the input position vector
+        // then subtract this from 90 degrees to shift it to be from the z-x plane
+        var y = (Math.PI/2) - Math.acos(yAxisVector.dot(position_vector));
 	    
-	    // calculate the angle between the input vector projected on the z-x plane and the z-axis
-	    var x = Math.acos(zAxisVector.dot(positionY0Vector));
+        // calculate the angle between the input vector projected on the z-x plane and the z-axis
+        var x = Math.acos(zAxisVector.dot(positionY0Vector));
 	    
-	    // since the above calculation will return between 0 and 180 degrees, invert it if we're in the 
-	    // negative x direction
-	    if(positionY0Vector.x < 0) {
-	        x = -x;
-	    }
+        // since the above calculation will return between 0 and 180 degrees, invert it if we're in the
+        // negative x direction
+        if(positionY0Vector.x < 0) {
+            x = -x;
+        }
 	    
-	    // set it to zero if NaN
-	    target.y = isNaN(y) ? 0 : y;
-	    target.x = isNaN(x) ? 0 : x;
+        // set it to zero if NaN
+        target.y = isNaN(y) ? 0 : y;
+        target.x = isNaN(x) ? 0 : x;
 	    
-	    logger.debug("target: " + JSON.stringify(target));
-	}
+        logger.debug("target: " + JSON.stringify(target));
+    }
 	
-	init();
+    init();
 
-	return this;
+    return this;
 
 };
