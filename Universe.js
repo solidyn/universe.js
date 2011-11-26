5ab783a46fa0fce7593380a2b465b5354060f065
Chris Rothe <crothe@solidyn.com>
2011-11-25 20:24:41 -0700

    added crude satellite roll model to just make them point at center of the earth at all times

--- Universe.js
+++ Universe.js
@@ -28,6 +28,8 @@ SSI.Universe = function(options, container) {
     var isShowGround = true;
     
     var universe = this;
+    
+    var earthCenterPoint = new THREE.Vector3(0,0,0);
 
     controller.addGraphicsObject({
         id : "simState",
@@ -179,6 +181,7 @@ SSI.Universe = function(options, container) {
             objectLibrary.getObjectById("default_material", function(retrieved_material) {
                 material = retrieved_material;
                 
+                objectGeometry.applyMatrix( new THREE.Matrix4().setRotationFromEuler( new THREE.Vector3( 0, Math.PI, 0 ) ));
                 var objectModel = new THREE.Mesh(objectGeometry, material);
         
                 controller.addGraphicsObject({
@@ -186,8 +189,11 @@ SSI.Universe = function(options, container) {
                     objectName : spaceObject.objectName,
                     update : function(elapsedTime) {
                         // need to pass a time to the propagator
-                            var convertedLocation = eciTo3DCoordinates(spaceObject.propagator(), true);
-                            objectModel.position.set(convertedLocation.x, convertedLocation.y, convertedLocation.z);
+                        var convertedLocation = eciTo3DCoordinates(spaceObject.propagator(), true);
+                        objectModel.position.set(convertedLocation.x, convertedLocation.y, convertedLocation.z);
+                        
+                        //http://mrdoob.github.com/three.js/examples/misc_lookat.html
+                        objectModel.lookAt(earthCenterPoint);
                     },
                     draw : function() {
                         core.draw(this.id, objectModel, false);
@@ -232,6 +238,7 @@ SSI.Universe = function(options, container) {
                         var position = eciTo3DCoordinates(groundObject.propagator());
                         groundObjectMesh.position.set(position.x, position.y, position.z);
 
+                        //http://mrdoob.github.com/three.js/examples/misc_lookat.html
                         var scaled_position_vector = new THREE.Vector3(position.x, position.y, position.z);
                         
                         // arbitrary size, just a point along the position vector further out for the object to lookAt
@@ -425,47 +432,4 @@ SSI.Universe = function(options, container) {
             z : location.y
         };
     }
-};
-//http://mrdoob.github.com/three.js/examples/misc_lookat.html
-// https://github.com/mrdoob/three.js/issues/382
-function lookat(vecstart,vecEnd,vecUp){
-
-    var temp = new THREE.Matrix4()
-    temp.lookAt(vecEnd,vecstart,vecUp);
-
-    var m00 = temp.n11, m10 = temp.n21, m20 = temp.n31,
-    m01 = temp.n12, m11 = temp.n22, m21 = temp.n32,
-    m02 = temp.n13, m12 = temp.n23, m22 = temp.n33;
-
-    var t = m00 + m11 + m22,s,x,y,z,w;
-
-    if (t > 0) { 
-      s =  Math.sqrt(t+1)*2; 
-      w = 0.25 * s;            
-      x = (m21 - m12) / s;
-      y = (m02 - m20) / s;
-      z = (m10 - m01) / s;
-    } else if ((m00 > m11) && (m00 > m22)) {
-      s =  Math.sqrt(1.0 + m00 - m11 - m22)*2;
-      x = s * 0.25;
-      y = (m10 + m01) / s;
-      z = (m02 + m20) / s;
-      w = (m21 - m12) / s;
-    } else if (m11 > m22) {
-      s =  Math.sqrt(1.0 + m11 - m00 - m22) *2; 
-      y = s * 0.25;
-      x = (m10 + m01) / s;
-      z = (m21 + m12) / s;
-      w = (m02 - m20) / s;
-    } else {
-      s =  Math.sqrt(1.0 + m22 - m00 - m11) *2; 
-      z = s * 0.25;
-      x = (m02 + m20) / s;
-      y = (m21 + m12) / s;
-      w = (m10 - m01) / s;
-    }
-
-    var rotation = new THREE.Quaternion(x,y,z,w);
-    rotation.normalize();
-    return rotation;
 };
