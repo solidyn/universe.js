e221d30510558ba44614c3630953b56f432b82c1
Chris Rothe <crothe@solidyn.com>
2011-12-07 23:25:15 -0700

    brought in utils from panda

--- utils/RSWCoordinates.js
+++
@@ -1,8 +0,0 @@
-function RSWCoordinates() {
- 
-    // define variables as <var name>: <value>
-
-    radial =     0.0, //radial vector (km)
-    alongTrack = 0.0, //along track vector (km)
-    crossTrack = 0.0  //cross track vector (km)
-};
