fb674a56da10da59365665fa936dd5d388e0b1dc
Chris Rothe <crothe@solidyn.com>
2011-12-07 23:22:26 -0700

    Moving to universejs repo

---
+++ utils/RSWCoordinates.js
@@ -0,0 +1,8 @@
+function RSWCoordinates() {
+ 
+    // define variables as <var name>: <value>
+
+    radial =     0.0, //radial vector (km)
+    alongTrack = 0.0, //along track vector (km)
+    crossTrack = 0.0  //cross track vector (km)
+};
