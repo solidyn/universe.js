673e8aa874fd7044925c325e285e7a4204a6e80c
Chris Rothe <crothe@solidyn.com>
2011-12-07 20:14:41 -0700

    renamed to universe.js

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-cat src/*.js > build/universe3d.js
+cat src/*.js > build/universe.js
 
-java -jar ~/Downloads/yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar -o build/universe3d-min.js build/universe3d.js
+java -jar ~/Downloads/yuicompressor-2.4.7/build/yuicompressor-2.4.7.jar -o build/universe-min.js build/universe.js
